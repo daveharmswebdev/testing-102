983ceb2277575d6a3c7de7050aee5ce197a1121b
Dave Harms <daveharmswebdev@gmail.com>
2016-10-17 11:15:12 -0500

    added route testing with route test passing

--- test/express.js
+++ test/express.js
@@ -1,7 +1,14 @@
 'use strict'
 
+const request = require('supertest')
+const app = require('../server')
+
 describe('express', () => {
-	it('should respond with hello world', () => {
-		// res.send === 'hello world'
+	it('should respond with hello world', (done) => {
+		request(app)
+			.get('/')
+			.expect(200)
+			.expect('hello world')
+			.end(done)
 	})
 })

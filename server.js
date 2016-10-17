6b37c60cc0d49ae729d3fad87597bddafbc3b6d8
Dave Harms <daveharmswebdev@gmail.com>
2016-10-17 13:55:59 -0500

    tested get post and delete routes

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 'use strict'
 
+// const { json } = require('body-parser')
+
 const express = require('express')
 const app = express()
 
+
+
 app.get('/', (req,res) => {
 	res.send('hello world')
+})
+
+app.get('/api/user', (req, res) => {
+	res.send({message: 'hello world'})
+})
+
+app.post('/api/address', (req,res) => {
+	res.status(201).send({_id:'123qwe'})
+})
+
+app.delete('/api/address/:id', (req,res) => {
+	res.send(204)
 })
 
 module.exports = app
--- test/express.js
+++ test/express.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { property } = require('chai').assert
 const request = require('supertest')
 const app = require('../server')
 
@@ -11,4 +12,40 @@ describe('express', () => {
 			.expect('hello world')
 			.end(done)
 	})
+
+	// should be json
+	it('should respond with json', (done) => {
+		request(app)
+		  .get('/api/user')
+		  .expect('Content-Type', /json/)
+		  .expect(200)
+		  .end(done)
+	})
+
+	// post to /address responds with 201 json
+	it('should post to address and respons with 201 json', (done) => {
+		request(app)
+			.post('/api/address')
+			.expect(201)
+			.expect('Content-Type', /json/)
+			.expect(res => property(res.body, '_id'))
+			.end(done)
+	})
+
+	// delete /addressess/:id 204
+	it('delete api/address/:id 204', (done) => {
+		request(app)
+			.delete('/api/address/100')
+			.expect(204)
+			.end(done)
+	})
 })
+
+
+
+
+
+
+
+
+
